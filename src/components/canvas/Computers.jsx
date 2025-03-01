a25d203b0e4c45ba07bcff0cb839c7f907eb31e4
SWETHACS17 <swetha.cs17@gmail.com>
2025-03-01 13:32:01 +0530

    updated computer size in mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -51,7 +51,7 @@ const Computers = ({ isMobile }) => {
 
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.60 : 0.65}
+        scale={isMobile ? 0.40 : 0.65}
         position={isMobile ? [0, -4.5, -0.5] : [0, -2.90, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
